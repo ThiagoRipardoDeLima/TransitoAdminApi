52978114ad074dadb321302f0c8f8817c490e180
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2024-09-18 21:02:23 -0400

    Customizando as mensagens de validação

--- src/main/java/dev/thiagoripardo/transito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/dev/thiagoripardo/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,13 +1,51 @@
 package dev.thiagoripardo.transito.api.exceptionhandler;
 
 import dev.thiagoripardo.transito.domain.exception.NegocioException;
+import lombok.AllArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou Mais campos estão inválidos");
+        problemDetail.setType(URI.create("http://localhos:8080/erros/campos-inválidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                                        .stream()
+                                        .collect(Collectors.toMap(
+                                                objectError -> ((FieldError) objectError).getField(),
+                                                objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())));
+
+        problemDetail.setProperty("fields", fields);
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e){

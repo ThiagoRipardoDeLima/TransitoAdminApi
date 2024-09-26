a327cd83372b6d019c94168d0c72a259104b93e6
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2024-09-25 20:11:54 -0400

    Compondo objetos no Representation Model

---
+++ src/main/java/dev/thiagoripardo/transito/api/model/ProprietarioResumoModel.java
@@ -0,0 +1,11 @@
+package dev.thiagoripardo.transito.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+    private long id;
+    private String nome;
+}

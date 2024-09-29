1051e1e31cb64da78b06d80fc4912c849bba0e9a
Thiago Ripardo de Lima <thiago.ripardo.86@gmail.com>
2024-09-29 12:15:58 -0400

    Implementando recurso de coleção de autuações

--- src/main/java/dev/thiagoripardo/transito/api/controller/AutuacaoController.java
+++ src/main/java/dev/thiagoripardo/transito/api/controller/AutuacaoController.java
@@ -4,11 +4,15 @@ import dev.thiagoripardo.transito.api.assembler.AutuacaoAssembler;
 import dev.thiagoripardo.transito.api.model.AutuacaoModel;
 import dev.thiagoripardo.transito.api.model.input.AutuacaoInput;
 import dev.thiagoripardo.transito.domain.model.Autuacao;
+import dev.thiagoripardo.transito.domain.model.Veiculo;
 import dev.thiagoripardo.transito.domain.service.RegistroAutuacaoService;
+import dev.thiagoripardo.transito.domain.service.RegistroVeiculoService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +21,7 @@ public class AutuacaoController {
 
     private final AutuacaoAssembler autuacaoAssembler;
     private final RegistroAutuacaoService registroAutuacaoService;
+    private final RegistroVeiculoService registroVeiculoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,6 +31,13 @@ public class AutuacaoController {
         Autuacao novaAutuacao = autuacaoAssembler.toEntity(autuacaoInput);
         Autuacao autuacaoRegistrada = registroAutuacaoService.registrar(veiculoId, novaAutuacao);
         return autuacaoAssembler.toModel(autuacaoRegistrada);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AutuacaoModel> listar(@PathVariable Long veiculoId){
+        Veiculo veiculo = registroVeiculoService.buscar(veiculoId);
+        return autuacaoAssembler.toCollectionModel(veiculo.getAutuacoes());
     }
 
 }
